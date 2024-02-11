753cc964f9619e84c8e07e70b857d70fe92de076
Aaron <aaronli5260@gmail.com>
2024-02-11 16:39:20 -0600

    awards fix

---
+++ helpers.py
@@ -0,0 +1,111 @@
+import requests
+from bs4 import BeautifulSoup
+from fuzzywuzzy import process
+import re
+
+# Get hotel name
+def extract_hotel_name(caption):
+    try:
+        return caption.find('span', class_='GallerySlideCaptionHedText-iqjOmM jwPuvZ').text
+    except AttributeError:
+        return None
+
+# Get Conde hotel link
+def extract_primary_hotel_link(caption):
+    try:
+        link_element = caption.find('a', class_='external-link')
+        if link_element:
+            return link_element['href']
+    except AttributeError:
+        return None
+
+# Get hotel details from Conde hotel link
+def scrape_hotel_details(hotel_link):
+    response = requests.get(hotel_link)
+    external_link = None
+    paragraphs = []
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract hotel website URL
+        address_wrapper = soup.find('div', class_='VenuePageVenueAddressWrapper-ezUZcT jJGvfj')
+        if address_wrapper:
+            address_details = address_wrapper.find('div', class_='VenueAddressDetails')
+            if address_details:
+                # Extracting external link
+                external_link_tag = address_details.find('a', class_='external-link')
+                
+                if external_link_tag and 'href' in external_link_tag.attrs:
+                    external_link = external_link_tag['href']
+
+                    # Now check for the existence of the specified div and class
+                    inner_container = soup.find('div', class_='container--body-inner')
+                    if inner_container:
+                        # Extracting all paragraphs within the specified div
+                        paragraphs = [paragraph.get_text(separator='\n').strip() for paragraph in inner_container.find_all('p')]
+                    else:
+                        print("Container for paragraphs not found.")
+                else:
+                    print("External link not found.")
+            else:
+                print("Venue address details not found.")
+        else:
+            print("Venue address wrapper not found.")
+    else:
+        print(f"Error: Unable to fetch the hotel page. Status code: {response.status_code}")
+
+    return external_link, paragraphs
+
+# Fuzzy match award name to known award categories
+def categorize_awards(award_name):
+    categories = ["Readers' Choice Awards", "Gold List", "Hot List"]
+    match, score = process.extractOne(award_name, categories)
+    
+    if score >= 80:
+        return match
+    else:
+        return 'Other'
+
+# Clean data for easier parsing
+def format_awards_list(awards_list):
+    formatted_awards_list = []
+
+    for award in awards_list:
+        matches = re.findall(r'(\D+)(\d+)', award)
+        if matches:
+            award_name = matches[0][0].strip()
+
+            # Fuzzy match award name for consistent style
+            award_name = categorize_awards(award_name)
+
+            years = [match[1] for match in matches]
+            for year in years:
+                formatted_awards_list.append(f"{award_name}, {year}.")
+
+    formatted_awards_string = ' '.join(formatted_awards_list)
+    return formatted_awards_string
+
+# Get awards list
+def extract_awards_list(detail_element):
+    try:
+        awards_text = detail_element.get_text(separator=' ').strip()
+        awards_list = awards_text.strip().split('   ')
+        cleaned_awards_list = [award for award in awards_list if '$' not in award]
+
+        # Process data
+        formatted_awards_list = format_awards_list(cleaned_awards_list)
+
+        # Counting the number of dollar signs
+        dollar_sign_count = awards_text.count('$')
+
+        return formatted_awards_list, dollar_sign_count
+    except (AttributeError, IndexError):
+        return None, 0
+
+# Get hotel description from Conde list
+def extract_description(caption):
+    try:
+        return caption.find('div', class_='GallerySlideCaptionDekContainer-hLUdt gSWuis').find('p').text
+    except AttributeError:
+        return None
--- main.py
+++ main.py
@@ -1,81 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-def extract_hotel_name(caption):
-    try:
-        return caption.find('span', class_='GallerySlideCaptionHedText-iqjOmM jwPuvZ').text
-    except AttributeError:
-        return None
-
-def extract_primary_hotel_link(caption):
-    try:
-        link_element = caption.find('a', class_='external-link')
-        if link_element:
-            return link_element['href']
-    except AttributeError:
-        return None
-
-def scrape_hotel_details(hotel_link):
-    response = requests.get(hotel_link)
-    external_link = None
-    paragraphs = []
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Extract hotel website URL
-        address_wrapper = soup.find('div', class_='VenuePageVenueAddressWrapper-ezUZcT jJGvfj')
-        if address_wrapper:
-            address_details = address_wrapper.find('div', class_='VenueAddressDetails')
-            if address_details:
-                # Extracting external link
-                external_link_tag = address_details.find('a', class_='external-link')
-                
-                if external_link_tag and 'href' in external_link_tag.attrs:
-                    external_link = external_link_tag['href']
-
-                    # Now check for the existence of the specified div and class
-                    inner_container = soup.find('div', class_='container--body-inner')
-                    if inner_container:
-                        # Extracting all paragraphs within the specified div
-                        paragraphs = [paragraph.get_text(separator='\n').strip() for paragraph in inner_container.find_all('p')]
-                    else:
-                        print("Container for paragraphs not found.")
-                else:
-                    print("External link not found.")
-            else:
-                print("Venue address details not found.")
-        else:
-            print("Venue address wrapper not found.")
-    else:
-        print(f"Error: Unable to fetch the hotel page. Status code: {response.status_code}")
-
-    return external_link, paragraphs
-
-def extract_awards_list(detail_element):
-    try:
-        awards_text = detail_element.get_text(separator=' ').strip()
-        awards_list_start = awards_text.find('$$$ | ') + len('$$$ | ')
-        cleaned_awards_list = awards_text[awards_list_start:].strip()
-
-        # Counting the number of dollar signs
-        dollar_sign_count = awards_text.count('$')
-
-        return cleaned_awards_list, dollar_sign_count
-    except (AttributeError, IndexError):
-        return None, 0
-
-def extract_description(caption):
-    try:
-        return caption.find('div', class_='GallerySlideCaptionDekContainer-hLUdt gSWuis').find('p').text
-    except AttributeError:
-        return None
+from helpers import extract_hotel_name, extract_primary_hotel_link, scrape_hotel_details, extract_awards_list, extract_description
 
 def scrape_conde():
     base_url = 'https://www.cntraveler.com'
 
-    # replace link here to scrape conde list
+    # Conde link to scrape
     url = 'https://www.cntraveler.com/gallery/best-hotels-in-los-angeles'
 
     response = requests.get(url)
@@ -83,9 +14,10 @@ def scrape_conde():
     data = {'Hotel Name': [], 'Awards': [], 'Price Point': [], 'Description (List)': [], 'Description (Hotel)': [], 'Hotel Website URL': []}
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
+        soup = BeautifulSoup(response.text, 'html.parser')    
+
         hotel_captions = soup.find_all('div', class_='GallerySlideFigCaption-dOeyTg jgkmHh')
+
         for caption in hotel_captions:
             # Extract the hotel name
             hotel_name = extract_hotel_name(caption)
@@ -118,6 +50,7 @@ def scrape_conde():
             # Extract other relevant information as needed
             description = extract_description(caption)
             data['Description (List)'].append(description)        
+            
     else:
         print(f"Error: Unable to fetch the page. Status code: {response.status_code}")
 
