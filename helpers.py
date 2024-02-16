c80f5bb4b276315f6a68f03c4f16b807829c81c2
Aaron <aaronli5260@gmail.com>
2024-02-15 23:45:34 -0600

    clean

--- helpers.py
+++ helpers.py
@@ -10,7 +10,7 @@ def extract_hotel_name(caption):
     except AttributeError:
         return None
 
-# Get Conde hotel link
+# Get hotel link
 def extract_primary_hotel_link(caption):
     try:
         link_element = caption.find('a', class_='external-link')
@@ -19,53 +19,51 @@ def extract_primary_hotel_link(caption):
     except AttributeError:
         return None
 
-# Get hotel details from Conde hotel link
+# Abstract parts of Beautiful Soup
+def extract_soup_from_response(response):
+    if response.status_code == 200:
+        return BeautifulSoup(response.text, 'html.parser')
+    else:
+        print(
+            f"Error: Unable to fetch the hotel page. Status code: {response.status_code}")
+        return None
+
+# Get hotel details
 def scrape_hotel_details(hotel_link):
     response = requests.get(hotel_link)
     external_link = ''
     paragraphs = []
     address = ''
+    soup = extract_soup_from_response(response)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Extract hotel website URL
-        address_wrapper = soup.find('div', class_='VenuePageVenueAddressWrapper-ezUZcT jJGvfj')
+    if soup:
+        address_wrapper = soup.find(
+            'div', class_='VenuePageVenueAddressWrapper-ezUZcT jJGvfj')
         if address_wrapper:
-            address_details = address_wrapper.find('div', class_='VenueAddressDetails')
+            # Get address data from Conde
+            address_details = address_wrapper.find(
+                'div', class_='VenueAddressDetails')
             if address_details:
-                # Extract venue address
-                address_element = address_details.find('div', class_='VenueAddress')
-                country_details = address_wrapper.find('div', {'data-testid': 'VenueAddressCountry'})
+                address_element = address_details.find(
+                    'div', class_='VenueAddress')
+                country_details = address_wrapper.find(
+                    'div', {'data-testid': 'VenueAddressCountry'})
                 if address_element:
                     address = address_element.text.strip()
                     if country_details:
                         country = country_details.text.strip()
-                        address = address + ", " + country
-                else:
-                    print("Address element not found.")
+                        address = f"{address}, {country}"
 
-                # Extracting external link
-                external_link_tag = address_details.find('a', class_='external-link')
-                
+                external_link_tag = address_details.find(
+                    'a', class_='external-link')
+
                 if external_link_tag and 'href' in external_link_tag.attrs:
                     external_link = external_link_tag['href']
-
-                    # Now check for the existence of the specified div and class
-                    inner_container = soup.find('div', class_='container--body-inner')
+                    inner_container = soup.find(
+                        'div', class_='container--body-inner')
                     if inner_container:
-                        # Extracting all paragraphs within the specified div
-                        paragraphs = [paragraph.get_text(separator='\n').strip() for paragraph in inner_container.find_all('p')]
-                    else:
-                        print("Container for paragraphs not found.")
-                else:
-                    print("External link not found.")
-            else:
-                print("Venue address details not found.")
-        else:
-            print("Venue address wrapper not found.")
-    else:
-        print(f"Error: Unable to fetch the hotel page. Status code: {response.status_code}")
+                        paragraphs = [paragraph.get_text(separator='\n').strip(
+                        ) for paragraph in inner_container.find_all('p')]
 
     return external_link, paragraphs, address
 
@@ -73,7 +71,7 @@ def scrape_hotel_details(hotel_link):
 def categorize_awards(award_name):
     categories = ["Readers' Choice Awards", "Gold List", "Hot List"]
     match, score = process.extractOne(award_name, categories)
-    
+
     if score >= 80:
         return match
     else:
@@ -103,7 +101,8 @@ def extract_awards_list(detail_element):
     try:
         awards_text = detail_element.get_text(separator=' ').strip()
         awards_list = awards_text.strip().split('   ')
-        cleaned_awards_list = [award for award in awards_list if '$' not in award]
+        cleaned_awards_list = [
+            award for award in awards_list if '$' not in award]
 
         # Process data
         formatted_awards_list = format_awards_list(cleaned_awards_list)
--- main.py
+++ main.py
@@ -1,71 +1,73 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from helpers import extract_hotel_name, extract_primary_hotel_link, scrape_hotel_details, extract_awards_list, extract_description
+from helpers import extract_soup_from_response, extract_hotel_name, extract_primary_hotel_link, scrape_hotel_details, extract_awards_list, extract_description
+
 
 def scrape_conde():
     base_url = 'https://www.cntraveler.com'
 
     # Conde link to scrape
-    url = 'https://www.cntraveler.com/gallery/best-hotels-in-buenos-aires'
+    url = 'https://www.cntraveler.com/gallery/best-hotels-in-london'
 
     response = requests.get(url)
+    soup = extract_soup_from_response(response)
 
-    data = {'Hotel Name': [], 'Awards': [], 'Price Point': [], 'Description (List)': [], 'Description (Hotel)': [], 'Address': [],'Conde Hotel URL': [], 'Hotel Website URL': []}
+    data = {'Hotel Name': [], 'Awards': [], 'Price Point': [], 'Description (List)': [
+    ], 'Description (Hotel)': [], 'Address': [], 'Conde Hotel URL': [], 'Hotel Website URL': []}
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')    
+    hotel_captions = soup.find_all(
+        'div', class_='GallerySlideFigCaption-dOeyTg jgkmHh')
 
-        hotel_captions = soup.find_all('div', class_='GallerySlideFigCaption-dOeyTg jgkmHh')
+    for caption in hotel_captions:
+        # Extract the hotel name
+        hotel_name = extract_hotel_name(caption)
+        print(hotel_name)
+        data['Hotel Name'].append(hotel_name)
 
-        for caption in hotel_captions:
-            # Extract the hotel name
-            hotel_name = extract_hotel_name(caption)
-            print(hotel_name)
-            data['Hotel Name'].append(hotel_name)
+        # Extract the primary hotel link
+        hotel_link = extract_primary_hotel_link(caption)
+        if (hotel_link):
+            primary_hotel_link = base_url + extract_primary_hotel_link(caption)
+        else:
+            primary_hotel_link = base_url
+        data['Conde Hotel URL'].append(primary_hotel_link)
 
-            # Extract the primary hotel link
-            hotel_link = extract_primary_hotel_link(caption)
-            if (hotel_link):
-                primary_hotel_link = base_url + extract_primary_hotel_link(caption)
-            else:
-                primary_hotel_link = base_url
-            data['Conde Hotel URL'].append(primary_hotel_link)
+        # Extract the detail element
+        detail_element = caption.find(
+            'div', class_='GallerySlideCaptionDetail-hSFZKt bPsOid')
 
-            # Extract the detail element
-            detail_element = caption.find('div', class_='GallerySlideCaptionDetail-hSFZKt bPsOid')
+        # Check if detail_element is not None before proceeding
+        if detail_element:
+            # Extract the awards list and count of dollar signs
+            awards_list, dollar_sign_count = extract_awards_list(
+                detail_element)
+            data['Awards'].append(awards_list)
+            data['Price Point'].append(dollar_sign_count)
+            external_link, paragraphs, address = scrape_hotel_details(
+                primary_hotel_link)
+            data['Address'].append(address)
+            data['Hotel Website URL'].append(external_link)
+            data['Description (Hotel)'].append('\n'.join(paragraphs))
 
-            # Check if detail_element is not None before proceeding
-            if detail_element:
-                # Extract the awards list and count of dollar signs
-                awards_list, dollar_sign_count = extract_awards_list(detail_element)
-                data['Awards'].append(awards_list)
-                data['Price Point'].append(dollar_sign_count)
-                external_link, paragraphs, address = scrape_hotel_details(primary_hotel_link)
-                data['Address'].append(address)
-                data['Hotel Website URL'].append(external_link)
-                data['Description (Hotel)'].append('\n'.join(paragraphs))
+        else:
+            print("Incomplete information: No detail element found.")
+            data['Awards'].append('')
+            data['Price Point'].append('')
+            data['Hotel Website URL'].append('')
+            data['Address'].append('')
+            data['Description (Hotel)'].append('')
 
-            else:
-                print("Incomplete information: No detail element found.")
-                data['Awards'].append('')
-                data['Price Point'].append('')
-                data['Hotel Website URL'].append('')
-                data['Address'].append('')
-                data['Description (Hotel)'].append('')
-
-            # Extract other relevant information as needed
-            description = extract_description(caption)
-            data['Description (List)'].append(description)        
-            
-    else:
-        print(f"Error: Unable to fetch the page. Status code: {response.status_code}")
+        # Extract other relevant information as needed
+        description = extract_description(caption)
+        data['Description (List)'].append(description)
 
     # Create a DataFrame from the data
     df = pd.DataFrame(data)
 
     # Save DataFrame to an Excel file
     df.to_excel('hotel_data.xlsx', index=False)
+
 
 if __name__ == "__main__":
     scrape_conde()
